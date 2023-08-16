2327419dde06880f73a2d2981dfb9eafe1aebce1
Alex Rossov <poccob86@gmail.com>
2023-08-16 11:29:38 +0800

    fix loginUser schema

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -1,5 +1,5 @@
 import { CounterSchema } from 'entities/Counter';
-import { UserSchema } from 'entities/user';
+import { UserSchema } from 'entities/User';
 
 export interface StateSchema {
   counter: CounterSchema;
