ad0ed323a35673ba84cbaf2f5d02458c6193af98
Alex Rossov <poccob86@gmail.com>
2023-08-18 16:37:09 +0800

    add login form

--- src/app/providers/StoreProvider/index.ts
+++ src/app/providers/StoreProvider/index.ts
@@ -1,5 +1,8 @@
 import { StoreProvider } from './ui/StoreProvider';
 import { createReduxStore } from './config/store';
 import type { StateSchema } from './config/StateSchema';
+import type { AppDispatch } from './config/store';
 
-export { StoreProvider, createReduxStore, StateSchema };
+export {
+  StoreProvider, createReduxStore, StateSchema, AppDispatch,
+};
